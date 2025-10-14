6b44a063a89f982bee0c638b3083452e44092a0f
schronding <schronding@gmail.com>
2025-10-14 16:42:37 -0600

    Cambio en vite.config.js para netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import {defineConfig} from "vite"
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [
-		
-	]
-})
+  plugins: [remix(), tsconfigPaths()],
+});
