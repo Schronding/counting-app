c3ebf46acb68de7170f5d4dd0dfd712f909de391
schronding <schronding@gmail.com>
2025-10-18 12:46:40 -0600

    Cambio a vite.config.js para incluir plugin react

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
-});
+  plugins: [react()],
+})
